84a42e929a03d5ffa1c418b20b1de81a28d63f44
thnhu <hnhu3050@gmail.com>
2025-04-05 19:48:35 +0700

    config root css

--- lotus-hotel/vite.config.js
+++ lotus-hotel/vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import path from 'path';
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    react()
   ],
+  resolve: {
+    alias: {
+      '@locales': path.resolve(__dirname, 'src/locales'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@layouts': path.resolve(__dirname, 'src/layouts'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@routes': path.resolve(__dirname, 'src/routes'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@images': path.resolve(__dirname, 'src/assets/images'),
+      '@fonts': path.resolve(__dirname, 'src/assets/fonts'),
+      '@api': path.resolve(__dirname, 'src/services/api'),
+    },
+  },
 })
